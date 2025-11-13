1d9ca6cf3657e043eb9c12f98b12c5af2e1c94e1
MeetAlica <feher.ali.96@gmail.com>
2025-11-14 00:42:33 +0100

    Modyify fetch

--- shoestore/src/App.jsx
+++ shoestore/src/App.jsx
@@ -24,7 +24,7 @@ export default function App() {
     const loadingToast = toast.loading("Sending your order...");
 
     try {
-      const res = await fetch("http://localhost:5000/api/order", {
+      const res = await fetch("backend-production-b39f.up.railway.app/api/order", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
