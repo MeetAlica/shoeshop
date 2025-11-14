454adebc6f217a5e656da527db909389e4a7ec3b
MeetAlica <feher.ali.96@gmail.com>
2025-11-14 17:21:50 +0100

    Fixing routes

--- backend/server.js
+++ backend/server.js
@@ -14,12 +14,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, "../shoestore/dist")));
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../shoestore/dist", "index.html"));
-});
-
 app.post("/api/order", async (req, res) => {
   const { name, email, sole, top } = req.body;
 
@@ -46,15 +40,20 @@ Sole: ${sole}
 Top: ${top}
 
 Your email: ${email}
-
       `,
     });
 
     res.json({ message: "Email sent successfully!" });
   } catch (err) {
     console.error(err);
-    res.status(500).json({ message: "An error ocured during sending the email." });
+    res.status(500).json({ message: "An error occurred while sending the email." });
   }
 });
 
-app.listen(process.env.PORT || 5000, () => console.log(`✅ Server running on port ${process.env.PORT || 5000}!`));
+app.use(express.static(path.join(__dirname, "../shoestore/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../shoestore/dist", "index.html"));
+});
+
+app.listen(process.env.PORT || 5000, () => console.log(`Server running on port ${process.env.PORT || 5000}`));
