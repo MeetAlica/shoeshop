e15d1ced32a21184d5cce848e9d7f1e1a405760f
MeetAlica <feher.ali.96@gmail.com>
2025-11-14 17:39:41 +0100

    Hotfix #3

--- backend/server.js
+++ backend/server.js
@@ -7,12 +7,14 @@ import dotenv from "dotenv";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+const frontendPath = path.join(__dirname, "dist");
 
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.static(frontendPath));
 
 app.post("/api/order", async (req, res) => {
   const { name, email, sole, top } = req.body;
@@ -50,7 +52,7 @@ Your email: ${email}
   }
 });
 
-app.use(express.static(path.join(__dirname, "../shoestore/dist")));
+app.use(express.static(path.join(frontendPath, "../shoestore/dist")));
 
 app.get(/.*/, (req, res) => {
   res.sendFile(path.join(__dirname, "../shoestore/dist", "index.html"));
